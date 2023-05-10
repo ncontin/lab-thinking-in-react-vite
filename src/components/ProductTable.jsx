cbd3c6f1baee942305bb564b1e4184ee19d48f6b
Nicolo Contin <nicocontin@gmail.com>
2023-05-10 18:01:20 +0200

    start iteration 2

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductRow from "./ProductRow";
+function ProductTable() {
+  return (
+    <div>
+      <h2>ProductTable</h2>
+      <ProductRow />
+    </div>
+  );
+}
+
+export default ProductTable;
